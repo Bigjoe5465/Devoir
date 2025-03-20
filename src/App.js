1c868dfd5c4c441ea6a73be0a4ee76f9ec0a62a6
Bigjoe5465 <cedricambroise2@gmail.com>
2025-03-19 22:50:15 -0400

    premier commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Hello from "./Hello"; // Import du second composant
+
+function App() {
+  return (
+    <div>
+      <h1>Hello World</h1>
+      <Hello name="Nova" />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Hello.js
@@ -0,0 +1,8 @@
+import React from "react";
+
+function Hello({ name }) {
+  return <p>Bonjour, {name} !</p>;
+}
+
+export default Hello;
+
